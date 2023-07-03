6aac95c0b66af0b191dcb38d20ebca5a38405f38
isamauny <isabelle.mauny@42crunch.com>
2023-07-03 22:58:58 +0200

    test setenv

---
+++ .42c/scripts/setenv.py
@@ -0,0 +1,8 @@
+import json
+import os
+
+def set_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
+
+set_output ("token1", "value1")
