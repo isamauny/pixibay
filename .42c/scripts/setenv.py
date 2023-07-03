8c8ea2532a5e5c8afe1f1293555fa1a9937ff60f
isamauny <isabelle.mauny@42crunch.com>
2023-07-03 23:09:51 +0200

    Fix typo

--- .42c/scripts/setenv.py
+++ .42c/scripts/setenv.py
@@ -1,8 +1,13 @@
 import json
 import os
 
-def set_output(name, value):
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+def set_env(name, value):
+    with open(os.environ['GITHUB_ENV'], 'a') as fh:
         print(f'{name}={value}', file=fh)
 
-set_output ("token1", "value1")
+def set_output(name, value):
+    with open(os.environ['GITHUB_OUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
+
+set_env ("token1", "env-value1")
+set_output ("token1", "output-value")
